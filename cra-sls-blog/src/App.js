8111812b85763a054f4ab1619514086235f100d3
Juliusz Jakubowski <juliusz.jakubowski+github@gmail.com>
2020-05-13 12:29:25 +0200

    simple routing to new post - no authorization for now

--- cra-sls-blog/src/App.js
+++ cra-sls-blog/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+
 import Home from './pages/Home'
+import NewPost from './pages/NewPost'
 
 function App() {
-  return (
-        <Home />
-  );
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/posts/new'>New Post</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route path='/posts/new'>
+                    <NewPost />
+                </Route>
+            </Switch>
+
+        </Router>
+    )
 }
 
-export default App;
+export default App
