0069c4875118f5af3b48c326a90d746e33125ac1
Juliusz Jakubowski <juliusz.jakubowski+github@gmail.com>
2020-05-13 12:28:43 +0200

    adding globalStyles on app level

--- cra-sls-blog/src/index.js
+++ cra-sls-blog/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import Amplify from 'aws-amplify'
 import aws_export from './aws-exports'
+import { GlobalStyles } from './index-styles'
 
 Amplify.configure(aws_export)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <GlobalStyles />
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
